1eb0383b9712171b21e6754defcb070e3b228477
JuanFTP <juan.tarazona@uniminuto.edu.co>
2025-02-19 16:37:00 -0500

    Datos Personales

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nombre:Juan Felipe Tarazona");
+        System.out.println("Carrera:ingenieria de sistemas");
+        System.out.println("ID:832837");
+        System.out.println("hobbie:League fo legends");
+        System.out.println("edad:");
+        }
+    }
